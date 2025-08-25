4a3b49c4eea681e0db287bf4e875fa353e314291
Vraj <vrajpatel40503@gmail.com>
2025-08-25 11:11:10 +0530

    change in express.js version

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  optionSuccessStatus:200,
 };
 
 console.log("Allowed origins:", allowedOrigins);
